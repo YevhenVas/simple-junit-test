9bb2bc8c2ce03c8365a675e581c3367ec272b4c8
obilyk <oleg.bilyk@onseo.biz>
2018-12-18 15:16:05 +0200

    Update pom and chromeDriver options

--- src/main/java/JustTest.java
+++ src/main/java/JustTest.java
@@ -2,26 +2,29 @@
  * Run simple selenium tests in Chrome browser
  */
 
-    import org.junit.*;
-    import org.openqa.selenium.By;
-    import org.openqa.selenium.WebDriver;
-    import org.openqa.selenium.WebElement;
-    import org.openqa.selenium.chrome.ChromeDriver;
-    import org.openqa.selenium.chrome.ChromeOptions;
-    import org.openqa.selenium.remote.DesiredCapabilities;
-    import java.util.HashMap;
-    import java.util.Map;
-    
-    //java -jar /usr/share/jenkins/jenkins.war
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.util.HashMap;
+import java.util.Map;
+
+//java -jar /usr/share/jenkins/jenkins.war
 
 public class JustTest {
-    String chromePath = "/drivers/chromedriver";
+    String chromePath = "/drivers/chromedriverWin.exe";
+    //String chromePath = "/drivers/chromedriverUnix";
+    //String chromePath = "/drivers/chromedriverMac";
     WebDriver driver;
 
     @Before
     public void startChrome() {
         setChromeEnvVariable();
-        driver = new ChromeDriver(getChromeCapabilities());
+        driver = new ChromeDriver(getChromeOptions());
+
     }
 
     @Test
@@ -56,12 +59,6 @@ public class JustTest {
         return System.getProperty("user.dir") + chromePath;
     }
 
-    private DesiredCapabilities getChromeCapabilities() {
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, getChromeOptions());
-        return capabilities;
-    }
-
     private ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("mobileEmulation", getMobileEmulation());
@@ -70,8 +67,38 @@ public class JustTest {
 
     private Map<String, String> getMobileEmulation() {
         Map<String, String> mobileEmulation = new HashMap<String, String>();
-        mobileEmulation.put("deviceName", "Google Nexus 10");
+        mobileEmulation.put("deviceName", "Nexus 10");
         return mobileEmulation;
     }
+    /*NOTE: Accepted devices:
+        BlackBerry Z30
+        Blackberry PlayBook
+        Galaxy Note 3
+        Galaxy Note II
+        Galaxy S III
+        Kindle Fire HDX
+        LG Optimus L70
+        Laptop with HiDPI screen
+        Laptop with MDPI screen
+        Laptop with touch
+        Microsoft Lumia 550
+        Microsoft Lumia 950
+        Nexus 10
+        Nexus 4
+        Nexus 5
+        Nexus 6
+        Nexus 7
+        Nokia Lumia 520
+        Nokia N9
+        iPad Mini
+        iPhone 4
+        Galaxy S5
+        Nexus 5X
+        Nexus 6P
+        iPhone 5
+        iPhone 6
+        iPhone 6 Plus
+        iPad
+    */
 
 }
