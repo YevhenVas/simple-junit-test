fb710b0d1052e4031abd5c5fbb79dbd77e90ddbd
YevhenVas <ievgenpet@gmail.com>
2019-03-01 20:42:17 +0200

    01.03.2019

--- src/main/java/AsService.java
+++ src/main/java/AsService.java
@@ -1,5 +1,37 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by max on 27.10.15.
  */
 public class AsService {
+
+
+    @Test
+    public void test() throws MalformedURLException {
+        DesiredCapabilities capa = new DesiredCapabilities();
+        capa.setCapability( "platformName", "Android");
+        capa.setCapability( "deviceName", "dfhjkk");
+        capa.setCapability( "appPackage", "ua.slando");
+        capa.setCapability( "appActivity", "pl.tablica2.app.startup.activity.StartupActivity");
+        capa.setCapability( "udid", "e0ab7515");
+
+        AppiumDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capa);
+
+
+
+        driver.findElement(By.id("ua.slando:id/dismissButton")).click();
+
+        driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
+        driver.findElement(By.id("ua.slando:id/search_src_text")).sendKeys("одежда");
+
+        ((WebElement) driver.findElements(By.id("ua.slando:id/photo")).get(0)).click();
+    }
 }
